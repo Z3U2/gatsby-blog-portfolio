73d7e6ce32818de0af83c8badf3a9afa620c32d1
z3u2 <ilyassnasr@gmail.com>
2019-03-08 14:01:42 +0100

    Add about page, update footer
    Added a sample about page, modified footer for better `<hr/>` placement

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,41 +2,43 @@ import React from "react"
 
 export default () => {
     return (
-        <footer>
-            <hr />
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-8 col-md-10 mx-auto">
-                        <ul className="list-inline text-center">
-                            <li className="list-inline-item">
-                                <a href="https://twitter.com">
-                                    <span className="fa-stack fa-lg">
-                                        <i className="fas fa-circle fa-stack-2x"></i>
-                                        <i className="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                                    </span>
-                                </a>
-                            </li>
-                            <li className="list-inline-item">
-                                <a href="https://facebook.com">
-                                    <span className="fa-stack fa-lg">
-                                        <i className="fas fa-circle fa-stack-2x"></i>
-                                        <i className="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                                    </span>
-                                </a>
-                            </li>
-                            <li className="list-inline-item">
-                                <a href="https://github.com">
-                                    <span className="fa-stack fa-lg">
-                                        <i className="fas fa-circle fa-stack-2x"></i>
-                                        <i className="fab fa-github fa-stack-1x fa-inverse"></i>
-                                    </span>
-                                </a>
-                            </li>
-                        </ul>
-                        <p className="copyright text-muted">Copyright &copy; Mohammed Ilyass Nasr</p>
+        <div>
+            <hr/>
+            <footer>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-8 col-md-10 mx-auto">
+                            <ul className="list-inline text-center">
+                                <li className="list-inline-item">
+                                    <a href="https://twitter.com">
+                                        <span className="fa-stack fa-lg">
+                                            <i className="fas fa-circle fa-stack-2x"></i>
+                                            <i className="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </a>
+                                </li>
+                                <li className="list-inline-item">
+                                    <a href="https://facebook.com">
+                                        <span className="fa-stack fa-lg">
+                                            <i className="fas fa-circle fa-stack-2x"></i>
+                                            <i className="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </a>
+                                </li>
+                                <li className="list-inline-item">
+                                    <a href="https://github.com">
+                                        <span className="fa-stack fa-lg">
+                                            <i className="fas fa-circle fa-stack-2x"></i>
+                                            <i className="fab fa-github fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </a>
+                                </li>
+                            </ul>
+                            <p className="copyright text-muted">Copyright &copy; Mohammed Ilyass Nasr</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </footer>
+            </footer>
+        </div>
     )
 }
--- src/conf.js
+++ src/conf.js
@@ -1,7 +1,9 @@
 // Overall page conf
 const config = {
     mainTitle : "My Blog",
-    mainSub : "Programming and technology enthusiast"
+    mainSub : "Programming and technology enthusiast",
+    aboutTitle : "About me",
+    aboutSub : "What I do"
 }
 
 export default config
