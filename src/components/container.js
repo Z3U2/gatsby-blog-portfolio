66dec11f621a57a3f54ecdd48c69b2c940e7be2d
z3u2 <ilyassnasr@gmail.com>
2019-09-24 21:53:14 +0100

    Change container size

--- src/components/container.js
+++ src/components/container.js
@@ -3,7 +3,7 @@ import React from "react"
 export default ({children}) => (
     <div className="container">
         <div className="row">
-            <div className="col-lg-8 col-md-10 mx-auto">
+            <div className="col-lg-10 col-md-10 mx-auto">
                 {children}
             </div>
         </div>
