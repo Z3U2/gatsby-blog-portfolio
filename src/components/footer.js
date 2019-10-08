80a6208d24a5bfe288ef65c87d71a3af529aa7eb
z3u2 <ilyassnasr@gmail.com>
2019-10-08 13:39:44 +0100

    Better social media choice

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,18 +10,10 @@ export default () => {
                         <div className="col-lg-8 col-md-10 mx-auto">
                             <ul className="list-inline text-center">
                                 <li className="list-inline-item">
-                                    <a href="https://twitter.com">
+                                    <a href="https://linkedin.com">
                                         <span className="fa-stack fa-lg">
                                             <i className="fas fa-circle fa-stack-2x"></i>
-                                            <i className="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                                        </span>
-                                    </a>
-                                </li>
-                                <li className="list-inline-item">
-                                    <a href="https://facebook.com">
-                                        <span className="fa-stack fa-lg">
-                                            <i className="fas fa-circle fa-stack-2x"></i>
-                                            <i className="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                                            <i className="fab fa-linkedin fa-stack-1x fa-inverse"></i>
                                         </span>
                                     </a>
                                 </li>
