6e62fbfc220321d34a181ba1f229a40bb0c2087f
z3u2 <ilyassnasr@gmail.com>
2019-10-09 16:39:25 +0100

    Update project-preview buttons

--- src/components/project-preview.js
+++ src/components/project-preview.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 export default ({ slug, title, subtitle, ...props }) => {
     return (
         <div className="post-preview col-lg-6 col-md-6 project-preview">
-            <img style ={{width:"100%", height:300}}>
+            <img style ={{width:"100%", height:300}} >
             </img>
             <Link to={`/${slug}/`}>
                 <h2 className="post-title">
@@ -14,7 +14,10 @@ export default ({ slug, title, subtitle, ...props }) => {
                     {subtitle}
                 </h3>
             </Link>
-            <Link className="btn btn-primary" to="/2">Check me &rarr;</Link>
+            <div className="project-buttons">
+                <Link className="btn btn-primary" to="/2">Code</Link>
+                <Link className="btn btn-primary" to="/2">Demo</Link>
+            </div>
             <hr />
         </div>
     )
