9faadc83cea3963fec3e1cc56da53218308e091e
z3u2 <ilyassnasr@gmail.com>
2019-10-11 03:09:11 +0100

    Add demo anc code links to project preview

--- src/components/project-preview.js
+++ src/components/project-preview.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
-export default ({ slug, title, subtitle, ...props }) => {
+export default ({ slug, title, subtitle, demo, code, skills, ...props }) => {
     return (
         <div className="post-preview col-lg-6 col-md-6 project-preview">
             <img style ={{width:"100%", height:300}} >
@@ -15,8 +15,8 @@ export default ({ slug, title, subtitle, ...props }) => {
                 </h3>
             </Link>
             <div className="project-buttons">
-                <Link className="btn btn-primary" to="/2">Code</Link>
-                <Link className="btn btn-primary" to="/2">Demo</Link>
+                <a className="btn btn-primary" href={code} target="_blank" rel="noopener noreferrer">Code</a>
+                <a className="btn btn-primary" href={demo} target="_blank" rel="noopener noreferrer">Demo</a>
             </div>
             <hr />
         </div>
