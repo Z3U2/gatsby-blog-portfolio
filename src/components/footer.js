54f50ff99984a9a96ce7cb90c8c1b1ca5689664a
z3u2 <ilyassnasr@gmail.com>
2019-10-11 03:10:04 +0100

    Handle cross origin links security issue

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,7 +12,7 @@ export default () => {
                         <div className="col-lg-8 col-md-10 mx-auto">
                             <ul className="list-inline text-center">
                                 <li className="list-inline-item">
-                                    <a href={conf.linkedin} target="_blank">
+                                    <a href={conf.linkedin} target="_blank" rel="noopener noreferrer">
                                         <span className="fa-stack fa-lg">
                                             <i className="fas fa-circle fa-stack-2x"></i>
                                             <i className="fab fa-linkedin fa-stack-1x fa-inverse"></i>
@@ -20,7 +20,7 @@ export default () => {
                                     </a>
                                 </li>
                                 <li className="list-inline-item">
-                                    <a href={conf.github} target="_blank">
+                                    <a href={conf.github} target="_blank" rel="noopener noreferrer">
                                         <span className="fa-stack fa-lg">
                                             <i className="fas fa-circle fa-stack-2x"></i>
                                             <i className="fab fa-github fa-stack-1x fa-inverse"></i>
