5e9ff8eab23cce538af2ac45f2774039e522ed0d
z3u2 <ilyassnasr@gmail.com>
2019-10-15 12:24:46 +0100

    Put footer in container

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,35 +4,39 @@ import conf from '../conf'
 
 export default () => {
     return (
-        <div>
-            <hr/>
-            <footer>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-8 col-md-10 mx-auto">
-                            <ul className="list-inline text-center">
-                                <li className="list-inline-item">
-                                    <a href={conf.linkedin} target="_blank" rel="noopener noreferrer">
-                                        <span className="fa-stack fa-lg">
-                                            <i className="fas fa-circle fa-stack-2x"></i>
-                                            <i className="fab fa-linkedin fa-stack-1x fa-inverse"></i>
-                                        </span>
-                                    </a>
-                                </li>
-                                <li className="list-inline-item">
-                                    <a href={conf.github} target="_blank" rel="noopener noreferrer">
-                                        <span className="fa-stack fa-lg">
-                                            <i className="fas fa-circle fa-stack-2x"></i>
-                                            <i className="fab fa-github fa-stack-1x fa-inverse"></i>
-                                        </span>
-                                    </a>
-                                </li>
-                            </ul>
-                            <p className="copyright text-muted">Copyright &copy; Mohammed Ilyass Nasr</p>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-10 col-md-10 mx-auto">
+                    <hr />
+                    <footer>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-lg-8 col-md-10 mx-auto">
+                                    <ul className="list-inline text-center">
+                                        <li className="list-inline-item">
+                                            <a href={conf.linkedin} target="_blank" rel="noopener noreferrer">
+                                                <span className="fa-stack fa-lg">
+                                                    <i className="fas fa-circle fa-stack-2x"></i>
+                                                    <i className="fab fa-linkedin fa-stack-1x fa-inverse"></i>
+                                                </span>
+                                            </a>
+                                        </li>
+                                        <li className="list-inline-item">
+                                            <a href={conf.github} target="_blank" rel="noopener noreferrer">
+                                                <span className="fa-stack fa-lg">
+                                                    <i className="fas fa-circle fa-stack-2x"></i>
+                                                    <i className="fab fa-github fa-stack-1x fa-inverse"></i>
+                                                </span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                    <p className="copyright text-muted">Copyright &copy; Mohammed Ilyass Nasr</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    </footer>
                 </div>
-            </footer>
+            </div>
         </div>
     )
 }
