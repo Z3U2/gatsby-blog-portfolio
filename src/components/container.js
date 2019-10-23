f455320bfc8be102c6c862d6c3ca56670cf30813
z3u2 <ilyassnasr@gmail.com>
2019-10-23 14:13:58 +0100

    Adjust container component width
    full width for medium screens

--- src/components/container.js
+++ src/components/container.js
@@ -3,7 +3,7 @@ import React from "react"
 export default ({children}) => (
     <div className="container">
         <div className="row">
-            <div className="col-lg-10 col-md-10 mx-auto">
+            <div className="col-lg-10 col-md-12 mx-auto">
                 {children}
             </div>
         </div>
