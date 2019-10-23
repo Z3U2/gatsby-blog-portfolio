0682c0901999550106a4c3f50946956c2f77614a
z3u2 <ilyassnasr@gmail.com>
2019-10-23 14:12:07 +0100

    Add resume download to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,24 +12,31 @@ export default () => {
                         <div className="container">
                             <div className="row">
                                 <div className="col-lg-8 col-md-10 mx-auto">
-                                    <ul className="list-inline text-center">
-                                        <li className="list-inline-item">
-                                            <a href={conf.linkedin} target="_blank" rel="noopener noreferrer">
-                                                <span className="fa-stack fa-lg">
-                                                    <i className="fas fa-circle fa-stack-2x"></i>
-                                                    <i className="fab fa-linkedin fa-stack-1x fa-inverse"></i>
-                                                </span>
-                                            </a>
-                                        </li>
-                                        <li className="list-inline-item">
-                                            <a href={conf.github} target="_blank" rel="noopener noreferrer">
-                                                <span className="fa-stack fa-lg">
-                                                    <i className="fas fa-circle fa-stack-2x"></i>
-                                                    <i className="fab fa-github fa-stack-1x fa-inverse"></i>
-                                                </span>
-                                            </a>
-                                        </li>
-                                    </ul>
+                                    <div className="row">
+                                        <div className="col-lg-6 col-md-6 col-xs-12 social-media-container">
+                                            <ul className="list-inline text-center">
+                                                <li className="list-inline-item">
+                                                    <a href={conf.linkedin} target="_blank" rel="noopener noreferrer">
+                                                        <span className="fa-stack fa-lg">
+                                                            <i className="fas fa-circle fa-stack-2x"></i>
+                                                            <i className="fab fa-linkedin fa-stack-1x fa-inverse"></i>
+                                                        </span>
+                                                    </a>
+                                                </li>
+                                                <li className="list-inline-item">
+                                                    <a href={conf.github} target="_blank" rel="noopener noreferrer">
+                                                        <span className="fa-stack fa-lg">
+                                                            <i className="fas fa-circle fa-stack-2x"></i>
+                                                            <i className="fab fa-github fa-stack-1x fa-inverse"></i>
+                                                        </span>
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <div className="col-lg-6 col-md-6 col-xs-12 resume-container">
+                                            <a href="/resume.pdf" id="resume" target="_blank">Download Resume</a>
+                                        </div>
+                                    </div>
                                     <p className="copyright text-muted">Copyright &copy; Mohammed Ilyass Nasr</p>
                                 </div>
                             </div>
