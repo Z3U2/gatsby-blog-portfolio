066dbf4abc8db6a4651f0b985af92814ead6cd3f
z3u2 <ilyassnasr@gmail.com>
2019-10-23 14:12:44 +0100

    Add project preview link and image

--- src/components/project-preview.js
+++ src/components/project-preview.js
@@ -1,19 +1,18 @@
 import React from "react"
-import { Link } from "gatsby"
 
-export default ({ slug, title, subtitle, demo, code, skills, ...props }) => {
+export default ({ slug, title, subtitle, demo, code, skills, image, ...props }) => {
     return (
         <div className="post-preview col-lg-6 col-md-6 project-preview">
-            <img style ={{width:"100%", height:300}} >
+            <img style ={{width:"100%", height:300, objectFit:"cover"}} src={`/projects/${image}.png`} >
             </img>
-            <Link to={`/${slug}/`}>
+            <a href={demo} target="_blank" rel="noopener noreferrer">
                 <h2 className="post-title">
                     {title}
                 </h2>
                 <h3 className="post-subtitle">
                     {subtitle}
                 </h3>
-            </Link>
+            </a>
             <div className="skills">
                 <p>
                     Skills : <br/> {skills}
